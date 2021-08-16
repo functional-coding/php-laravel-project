6cef895373e7182794f677de6e547b6abc140d8e
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-16 15:12:38 +0900

    add and update base files

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -2,16 +2,30 @@
 
 namespace App\Http;
 
-use FunctionalCoding\Illuminate\Http\ControllerTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Request;
 
-class Controller extends BaseController
+class Controller extends \Illuminate\Routing\Controller
 {
     use AuthorizesRequests;
-    use ControllerTrait;
     use DispatchesJobs;
     use ValidatesRequests;
+
+    public static function bearerToken()
+    {
+        return Request::bearerToken() ? Request::bearerToken() : '';
+    }
+
+    public static function input($key)
+    {
+        return Arr::get(Request::all(), $key, '');
+    }
+
+    public static function route($key)
+    {
+        return Request::route($key);
+    }
 }
