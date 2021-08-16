e9be3f7ce8262a45425df912f0fe322b6a6dae5e
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-16 15:05:28 +0900

    change middleware namespace to app/Http/Middlewares

--- app/Http/Middlewares/EncryptCookies.php
+++ app/Http/Middlewares/EncryptCookies.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Http\Middlewares;
 
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
--- app/Http/Middlewares/VerifyCsrfToken.php
+++ app/Http/Middlewares/VerifyCsrfToken.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Http\Middlewares;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
